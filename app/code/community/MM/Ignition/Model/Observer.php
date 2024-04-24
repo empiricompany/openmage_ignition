d0074452c20b55ed154ef2231fffd2dd316b66cd
Tony <tonyweboss@gmail.com>
2024-04-24 13:03:46 +0200

    fix event

--- app/code/community/MM/Ignition/Model/Observer.php
+++ app/code/community/MM/Ignition/Model/Observer.php
@@ -1,7 +1,7 @@
 <?php
 class MM_Ignition_Model_Observer extends Mage_Core_Model_Observer
 {
-    public function handleException(Varien_Event_Observer $observer)
+    public function handlePrintExceptionBefore(Varien_Event_Observer $observer)
     {
         $e = $observer->getEvent()->getException();
         \Spatie\Ignition\Ignition::make()
