f1bfc6e8f1fd926a418cd18717bb6225848edd65
Tony <tonyweboss@gmail.com>
2024-04-24 13:19:57 +0200

    wip

--- app/code/community/MM/Ignition/Model/Observer.php
+++ app/code/community/MM/Ignition/Model/Observer.php
@@ -1,12 +1,24 @@
 <?php
 class MM_Ignition_Model_Observer extends Mage_Core_Model_Observer
 {
+    public function handleAppInitBefore(Varien_Event_Observer $observer)
+    {
+        if( !Mage::getIsDeveloperMode() ) return;
+
+        $observer->getEvent()->getApp()->setErrorHandler(NULL);
+        \Spatie\Ignition\Ignition::make()
+                ->applicationPath(Mage::getBaseDir())
+                ->register();
+    }
+    
     public function handlePrintExceptionBefore(Varien_Event_Observer $observer)
     {
+        if( !Mage::getIsDeveloperMode() ) return;
+
         $e = $observer->getEvent()->getException();
         \Spatie\Ignition\Ignition::make()
             ->applicationPath(Mage::getBaseDir())
-            ->setTheme('dark')
+            ->useDarkMode()
             ->handleException($e);
         die();
     }
