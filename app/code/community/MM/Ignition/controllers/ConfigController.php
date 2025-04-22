bdd6cb69d7ab69198fcf513993dfea03ec2ba988
Sven Reichel <github-sr@hotmail.com>
2025-04-23 01:20:40 +0200

    Update

--- app/code/community/MM/Ignition/controllers/ConfigController.php
+++ app/code/community/MM/Ignition/controllers/ConfigController.php
@@ -1,20 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 class MM_Ignition_ConfigController extends Mage_Core_Controller_Front_Action
 {
-
-    public function updateAction()
+    /**
+     * @throws Zend_Controller_Response_Exception
+     */
+    public function updateAction(): void
     {
-        $result = $this->updateConfig($this->getRequest()->getParams());
+        /** @var array<string> $params */
+        $params = $this->getRequest()->getParams();
+        $result = $this->updateConfig($params);
+        $response = $this->getResponse();
         if (!$result) {
-            $this->getResponse()->setHttpResponseCode(400);
+            $response->setHttpResponseCode(400);
         }
-        
-        $this->getResponse()->setHeader('Content-type', 'application/json');
-        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
+
+        $response->setHeader('Content-type', 'application/json');
+        $response->setBody(Mage::helper('core')->jsonEncode($result));
     }
 
-    private function updateConfig($config)
+    /**
+     * @param array<string> $config
+     */
+    private function updateConfig(array $config): bool
     {
         /** @var MM_Ignition_Helper_Data $_helper */
         $_helper = Mage::helper('mm_ignition');
